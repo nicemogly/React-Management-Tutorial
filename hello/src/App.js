765b46c8bc9643464b22806db5716ad060f31e81
nicemogly <newmogly@naver.com>
2022-07-29 10:48:55 +0900

    Update App.js

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Customer from './Components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React44445
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '윤장훈1',
+  'birthday' : '751205' ,
+  'gender' : '남자' ,
+  'job' : '대학생'
+  },
+  {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '윤장훈2',
+  'birthday' : '7512052' ,
+  'gender' : '남자2' ,
+  'job' : '대학생2'
+  },
+  {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '윤장훈3',
+  'birthday' : '7512053' ,
+  'gender' : '남자3' ,
+  'job' : '대학생3'
+  }
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        { 
+          customers.map(c => { 
+           return ( 
+                   <Customer 
+                    key={c.id} 
+                    id={c.id} 
+                    name={c.name} 
+                    image={c.image} 
+                    birthday={c.birthday} 
+                    gender = {c.gender}
+                    job={c.job}
+                    />
+                  ) 
+           }
+          )
+      }
+      </div>
+          
+    );
+  }
+
 }
+
+
+
 
 export default App;
